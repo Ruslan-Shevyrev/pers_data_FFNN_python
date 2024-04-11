f0261f0dea6e6e26260e9d898c40dd82f9ed6bdb
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-04-11 09:08:28 +0300

    add first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import ffnn as nn
+
+# nn.training_new_model(output_model='FFNN_pers_data_100000')
+
+# nn.training_exists_model(input_model='FFNN_pers_data_1900000', output_model='FFNN_pers_data_2200000')
+
+nn.test_predict(model_name='FFNN_pers_data_2200000.h5')
+
+# nn.convert_to_h5(input_model='FFNN_pers_data_2200000')
